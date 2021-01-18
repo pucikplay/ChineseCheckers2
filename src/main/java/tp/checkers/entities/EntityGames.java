97b672d9c52d726f3620a01eb1fb035204c6d28f
tslipa <p.cegiela@outlook.com>
2021-01-18 18:17:25 +0100

    Added connection to the database via Hibernate.

---
+++ src/main/java/tp/checkers/entities/EntityGames.java
@@ -0,0 +1,78 @@
+package tp.checkers.entities;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "games", schema = "chinesecheckers", catalog = "")
+public class EntityGames {
+    private int gameId;
+    private Date startDate;
+    private Integer numberOfPlayers;
+    private Integer sizeOfBase;
+
+    @Id
+    @Column(name = "game_id")
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    @Basic
+    @Column(name = "start_date")
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    @Basic
+    @Column(name = "number_of_players")
+    public Integer getNumberOfPlayers() {
+        return numberOfPlayers;
+    }
+
+    public void setNumberOfPlayers(Integer numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+    }
+
+    @Basic
+    @Column(name = "size_of_base")
+    public Integer getSizeOfBase() {
+        return sizeOfBase;
+    }
+
+    public void setSizeOfBase(Integer sizeOfBase) {
+        this.sizeOfBase = sizeOfBase;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EntityGames that = (EntityGames) o;
+
+        if (gameId != that.gameId) return false;
+        if (startDate != null ? !startDate.equals(that.startDate) : that.startDate != null) return false;
+        if (numberOfPlayers != null ? !numberOfPlayers.equals(that.numberOfPlayers) : that.numberOfPlayers != null)
+            return false;
+        if (sizeOfBase != null ? !sizeOfBase.equals(that.sizeOfBase) : that.sizeOfBase != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = gameId;
+        result = 31 * result + (startDate != null ? startDate.hashCode() : 0);
+        result = 31 * result + (numberOfPlayers != null ? numberOfPlayers.hashCode() : 0);
+        result = 31 * result + (sizeOfBase != null ? sizeOfBase.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/tp/checkers/entities/EntityMoves.java
@@ -0,0 +1,89 @@
+package tp.checkers.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "moves", schema = "chinesecheckers", catalog = "")
+public class EntityMoves {
+    private int moveId;
+    private Integer iOrigin;
+    private Integer jOrigin;
+    private Integer iDestination;
+    private Integer jDestination;
+
+    @Id
+    @Column(name = "move_id")
+    public int getMoveId() {
+        return moveId;
+    }
+
+    public void setMoveId(int moveId) {
+        this.moveId = moveId;
+    }
+
+    @Basic
+    @Column(name = "I_origin")
+    public Integer getiOrigin() {
+        return iOrigin;
+    }
+
+    public void setiOrigin(Integer iOrigin) {
+        this.iOrigin = iOrigin;
+    }
+
+    @Basic
+    @Column(name = "J_origin")
+    public Integer getjOrigin() {
+        return jOrigin;
+    }
+
+    public void setjOrigin(Integer jOrigin) {
+        this.jOrigin = jOrigin;
+    }
+
+    @Basic
+    @Column(name = "I_destination")
+    public Integer getiDestination() {
+        return iDestination;
+    }
+
+    public void setiDestination(Integer iDestination) {
+        this.iDestination = iDestination;
+    }
+
+    @Basic
+    @Column(name = "J_destination")
+    public Integer getjDestination() {
+        return jDestination;
+    }
+
+    public void setjDestination(Integer jDestination) {
+        this.jDestination = jDestination;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        EntityMoves that = (EntityMoves) o;
+
+        if (moveId != that.moveId) return false;
+        if (iOrigin != null ? !iOrigin.equals(that.iOrigin) : that.iOrigin != null) return false;
+        if (jOrigin != null ? !jOrigin.equals(that.jOrigin) : that.jOrigin != null) return false;
+        if (iDestination != null ? !iDestination.equals(that.iDestination) : that.iDestination != null) return false;
+        if (jDestination != null ? !jDestination.equals(that.jDestination) : that.jDestination != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = moveId;
+        result = 31 * result + (iOrigin != null ? iOrigin.hashCode() : 0);
+        result = 31 * result + (jOrigin != null ? jOrigin.hashCode() : 0);
+        result = 31 * result + (iDestination != null ? iDestination.hashCode() : 0);
+        result = 31 * result + (jDestination != null ? jDestination.hashCode() : 0);
+        return result;
+    }
+}
